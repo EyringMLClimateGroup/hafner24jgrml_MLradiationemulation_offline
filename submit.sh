a7a79d210c7ba64e0d1f74388790c480d23df09e
katharinahafner <70482360+katharinahafner@users.noreply.github.com>
2024-11-11 13:56:44 +0000

    clean up and fixed typos

--- submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#sbatch ./submit_levante_gpu.sh nn_config/SW_HR.yml
-#sbatch ./submit_levante_gpu.sh nn_config/LW_HR.yml
-sbatch ./submit_levante_gpu.sh nn_config/SW_FLUX.yml
-sbatch ./submit_levante_gpu.sh nn_config/LW_FLUX.yml
